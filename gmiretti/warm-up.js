473afa5f166f21d0db9f66df3bf6c37bf68b389c
Gabriel Miretti <gmiretti@ascentio.com.ar>
2017-04-07 11:07:49 -0300

    Ejercicios resueltos en el workshop

---
+++ gmiretti/warm-up.js
@@ -0,0 +1,10 @@
+'use strict';
+// (() => {
+//     setTimeout(() => {
+//         console.log('TIMED OUT!');
+//     }, 300);
+// }());
+
+setTimeout(() => {
+    console.log('TIMED OUT!');
+}, 300);
